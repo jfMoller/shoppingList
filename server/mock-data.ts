6028ff611fc2d18d9b291f66d0653931e86ad2c6
jm <johan.moller040@gmail.com>
2023-04-09 16:31:57 +0200

    working on item toggle function

--- server/mock-data.ts
+++ server/mock-data.ts
@@ -1,38 +1,64 @@
 export let items = [
-    {
-      name: "Cheeseburger",
-      ingredients: ["beef patty", "cheese", "lettuce", "tomato", "onion", "ketchup", "mayonnaise", "bun"],
-      category: "main",
-      price: 8.99
-    },
-    {
-      name: "French Fries",
-      ingredients: ["potatoes", "salt", "pepper"],
-      category: "condiment",
-      price: 2.99
-    },
-    {
-      name: "Soda",
-      ingredients: ["carbonated water", "sugar", "flavoring"],
-      category: "drink",
-      price: 1.99
-    },
-    {
-      name: "Caesar Salad",
-      ingredients: ["romaine lettuce", "parmesan cheese", "croutons", "Caesar dressing"],
-      category: "main",
-      price: 10.99
-    },
-    {
-      name: "Ketchup",
-      ingredients: ["tomatoes", "vinegar", "sugar", "spices"],
-      category: "condiment",
-      price: 0.99
-    },
-    {
-      name: "Lemonade",
-      ingredients: ["lemons", "sugar", "water"],
-      category: "drink",
-      price: 2.49
-    }
-  ]
+  {
+    _id: "ERRWEGG3W412553232",
+    name: "Cheeseburger",
+    ingredients: [
+      "beef patty",
+      "cheese",
+      "lettuce",
+      "tomato",
+      "onion",
+      "ketchup",
+      "mayonnaise",
+      "bun",
+    ],
+    category: "main",
+    price: 8.99,
+    isCompleted: false,
+  },
+  {
+    _id: "ERRWEGG3W4WFWFW231",
+    name: "French Fries",
+    ingredients: ["potatoes", "salt", "pepper"],
+    category: "condiment",
+    price: 9.24,
+    isCompleted: false,
+  },
+  {
+    _id: "VERRG4563W412553232",
+    name: "Soda",
+    ingredients: ["carbonated water", "sugar", "flavoring"],
+    category: "drink",
+    price: 5.99,
+    isCompleted: false,
+  },
+  {
+    _id: "ELDKDHRUW412553232",
+    name: "Caesar Salad",
+    ingredients: [
+      "romaine lettuce",
+      "parmesan cheese",
+      "croutons",
+      "Caesar dressing",
+    ],
+    category: "main",
+    price: 12.22,
+    isCompleted: false,
+  },
+  {
+    _id: "ERRWELSK3421553232",
+    name: "Ketchup",
+    ingredients: ["tomatoes", "vinegar", "sugar", "spices"],
+    category: "condiment",
+    price: 14.33,
+    isCompleted: false,
+  },
+  {
+    _id: "ERR883728212553232",
+    name: "Lemonade",
+    ingredients: ["lemons", "sugar", "water"],
+    category: "drink",
+    price: 13.44,
+    isCompleted: false,
+  },
+];
--- src/shopping-list/ShoppingList.tsx
+++ src/shopping-list/ShoppingList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useState } from "react";
 import { items } from "../../server/mock-data";
 
@@ -11,38 +11,60 @@ interface Item {
 
 export default function ShoppingList() {
   const [allItems, setAllItems] = useState<Item[]>([]);
-
   useEffect(() => {
     setAllItems(items);
-  }, [items]);
+  }, []);
+
+  function addItem(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    console.log("add item");
+  }
+  function toggleItem(event: React.MouseEvent<HTMLSpanElement>, _id: string) {
+    event.preventDefault();
+    const updatedItems = allItems.map((item) => {
+      if (!item.isCompleted) {
+        return { ...item, isCompleted: true };
+      } else {
+        return { ...item, isCompleted: false };
+      }
+    });
+    setAllItems(updatedItems);
+    console.log(allItems)
+  }
+  function editItem(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    console.log("edit item");
+  }
+  function removeItem(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    console.log("remove item");
+  }
 
   const listItems = allItems.map((item) => {
+    const itemClass = (item.IsCompleted)
+      ? "flex-1 cursor-pointer hover:text-blue-500 w-5 line-through text-gray-400"
+      : "flex-1 cursor-pointer hover:text-blue-500 w-5";
+
     return (
       <li
         key={item.name}
         className="flex items-center py-4 border-b border-gray-300 hover:bg-gray-100 transition-colors duration-200 ease-in-out"
       >
         <span
-          className="flex-1 cursor-pointer hover:text-blue-500 w-5"
-          onClick={() => {
-            console.log("click1");
-          }}
+          className={itemClass}
+          onClick={(event) => toggleItem(event, item._id)}
         >
           {item.name}
         </span>
         <button
           className="ml-4 text-sm font-semibold text-green-500 hover:text-green-700 transition-colors duration-200 ease-in-out"
-          onClick={() => {
-            console.log("click2");
-          }}
+          onClick={editItem}
         >
           <i className="fa-solid fa-pen-to-square"></i>
         </button>
         <button
           className="ml-4 text-sm font-semibold text-red-500 hover:text-red-700 transition-colors duration-200 ease-in-out"
-          onClick={() => {
-            console.log("click3");
-          }}
+          onClick={removeItem}
         >
           <i className="fa-solid fa-xmark"></i>
         </button>
@@ -52,9 +74,15 @@ export default function ShoppingList() {
 
   return (
     <section className="h-screen max-w-3xl mx-auto w-[15rem] flex flex-col justify-center bg-gray-50 rounded-lg shadow-lg p-4">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">My Shopping List</h1>
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">
+        My Shopping List
+      </h1>
+
       <ul className="space-y-4">{listItems}</ul>
-      <button className="mt-8 py-2 px-4 bg-gradient-to-r from-teal-400 to-blue-500 hover:bg-gradient-to-r hover:from-teal-500 hover:to-blue-600 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out">
+      <button
+        onClick={addItem}
+        className="mt-8 py-2 px-4 bg-gradient-to-r from-teal-400 to-blue-500 hover:bg-gradient-to-r hover:from-teal-500 hover:to-blue-600 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out"
+      >
         Add Item
       </button>
     </section>

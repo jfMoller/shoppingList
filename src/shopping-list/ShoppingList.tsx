d90a03128f84eaa3cddb2f80cd58be60320f70cb
jm <johan.moller040@gmail.com>
2023-04-12 08:10:58 +0200

    some styling for phone viewport

--- src/shopping-list/ShoppingList.tsx
+++ src/shopping-list/ShoppingList.tsx
@@ -35,7 +35,7 @@ export default function ShoppingList() {
       };
       let updatedItems = [...allItems, newItem];
       setAllItems(updatedItems);
-      setInputValue("")
+      setInputValue("");
     }
   }
   function toggleItem(event: React.MouseEvent<HTMLSpanElement>, _id: string) {
@@ -89,44 +89,49 @@ export default function ShoppingList() {
         >
           {item.name}
         </span>
+
         <button
           className="ml-4 text-sm font-semibold text-green-500 hover:text-green-700 transition-colors duration-200 ease-in-out"
           onClick={(event) => editItem(event, item._id)}
         >
-          <i className="fa-solid fa-pen-to-square"></i>
+          <i className="fa-solid fa-pen-to-square ml-[-3rem]"></i>
         </button>
         <button
           className="ml-4 text-sm font-semibold text-red-500 hover:text-red-700 transition-colors duration-200 ease-in-out"
           onClick={(event) => removeItem(event, item._id)}
         >
-          <i className="fa-solid fa-xmark"></i>
+          <i className="fa-solid fa-xmark ml-[-2.5rem]"></i>
         </button>
+   
       </li>
     );
   });
 
   return (
     <section className="h-screen w-screen flex flex-col items-center bg-gray-50 rounded-lg shadow-lg p-4">
-  <h1 className="text-2xl font-bold text-gray-800 mb-4">
-    My Shopping List
-  </h1>
-  <form onSubmit={(event) => addItem(event)} className="flex items-center w-full">
-    <input
-      type="text"
-      value={inputValue}
-      onChange={handleInputChange}
-      placeholder="Enter an item..."
-      className="border border-gray-400 px-2 py-1 rounded-md w-full text-black focus:outline-none"
-    />
-    <button
-      type="submit"
-      className="ml-4 py-2 px-4 bg-gradient-to-r from-teal-400 to-blue-500 hover:bg-gradient-to-r hover:from-teal-500 hover:to-blue-600 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out"
-    >
-      Add Item
-    </button>
-  </form>
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">
+        My Shopping List
+      </h1>
+      <form
+        onSubmit={(event) => addItem(event)}
+        className="flex items-center w-full max-w-lg"
+      >
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter an item..."
+          className="border border-gray-400 px-2 py-1 rounded-md w-full text-black focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="ml-4 py-2 px-4 bg-gradient-to-r from-teal-400 to-blue-500 hover:bg-gradient-to-r hover:from-teal-500 hover:to-blue-600 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out"
+        >
+          Add
+        </button>
+      </form>
 
-  <ul className="mt-4 w-full max-w-lg">{listItems}</ul>
-</section>
+      <ul className="mt-4 w-full max-w-lg">{listItems}</ul>
+    </section>
   );
 }

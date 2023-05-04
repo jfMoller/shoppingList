150bcb53d02a731258fd5a5065e2fb84787d3dcf
jm <johan.moller040@gmail.com>
2023-05-04 18:14:26 +0200

    edits

--- src/calender/PageHeader.tsx
+++ src/calender/PageHeader.tsx
@@ -15,16 +15,7 @@ export function PageHeader(props: { caption: string, description: string, action
                         {props.description}
                     </p>
                 </div>
-                {props.onClick &&
-                  <div className="mt-4 mr-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                    <div onClick={props.onClick}
-                         className="inline-flex items-center justify-center rounded-md border border-transparent bg-green-800 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                    >
-                        {props.actionButton || "Reload"}
-                    </div>
-                  </div>
-                }
-            </div>
+              
         </div>
     </div>;
 }
